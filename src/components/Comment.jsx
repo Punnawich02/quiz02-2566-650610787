dc5a5d7421e496d47900f63df04270d70153c27a
Punnawich Mai <punnawich_dachin@gmail.com>
2023-08-10 14:51:13 +0700

    Finished

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,3 +1,7 @@
+// make comment component completed
+"use client";
+import { Reply } from "./Reply";
+
 export const Comment = ({
   userImagePath,
   username,
@@ -7,8 +11,41 @@ export const Comment = ({
 }) => {
   return (
     <div>
-      your code here...
       {/* map-loop render Reply component here */}
+      <div className="d-flex gap-2 my-2">
+          <img
+            src={userImagePath}
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#3A3B3C" }}
+          >
+            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+              {username}
+            </span>
+            <br />
+            <span style={{ color: "#E4E6EB" }}>{commentText}</span>
+            <div className="d-flex align-items-center gap-1">
+              {/* Like condition */}
+              {likeNum>0 ? <img src="/like.svg" width={20}></img> : ""} 
+              {likeNum>0 ? <span style={{ color: "#B0B3B8" }}>{likeNum} คน</span> : ""}
+            </div>
+          </div>
+        </div>
+        
+        {/* Show reply by mapping */}
+        {replies.map((user) => (
+          <Reply
+            userImagePath={user.userImagePath}
+            username={user.username}
+            replyText={user.replyText}
+            likeNum={user.likeNum}/>
+        ))}
+    
     </div>
   );
 };
--- src/components/PostOwner.jsx
+++ src/components/PostOwner.jsx
@@ -1,3 +1,37 @@
-export const PostOwner = ({}) => {
-  return <div>Your code here...</div>;
-};
+// Make Post Owner to component completed
+"use client";
+export const PostOwner = ({
+  userImgPath,
+  username,
+  postText,
+  likeNum
+}) => {
+  return (
+    <div className="vstack gap-3">
+    <div className="d-flex align-items-center gap-3">
+      <img
+        src={userImgPath}
+        width="48"
+        height="48"
+        className="rounded-circle"
+        style={{ objectFit: "cover" }}
+      />
+      <span className="fw-semibold fs-5 text-white">
+        {username}
+      </span>
+    </div>
+
+    <span className="text-white">
+      {postText}
+    </span>
+
+    <div className="d-flex align-items-center gap-1">
+      
+      {/* Like condition */}
+      {likeNum>0 ? <img src="/like.svg" width={20}></img> : ""}
+      {likeNum>0 ? <span style={{ color: "#B0B3B8" }}>{likeNum} คน</span> : ""}
+    </div>
+    <hr className="m-0 border" />
+  </div>
+  
+)};
